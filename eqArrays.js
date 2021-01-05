464402205663d1aba8e8433f0c90f656fcaf5177
Isaac King <idrkings@gmail.com>
2021-01-05 23:20:56 +0000

    Removed test cases

--- eqArrays.js
+++ eqArrays.js
@@ -14,14 +14,3 @@ const eqArrays = (firstArray, secondArray) => {
 
   return true;
 };
-
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected} ❌`);
-  }
-};
-
-
-assertEqual(eqArrays(['1', 2, 3], [1, 2, 3]), true);
