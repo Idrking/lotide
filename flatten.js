966d0497972b3a42f13cc83ebcd86e965865543c
Isaac King <idrkings@gmail.com>
2021-01-06 01:29:10 +0000

    Function the flattens an array with nested elements and returns a single array

---
+++ flatten.js
@@ -0,0 +1,12 @@
+const flatten = (arrayToFlatten) => {
+  for (let i = 0; i < arrayToFlatten.length; i++) {
+    if (Array.isArray(arrayToFlatten[i])) {
+      let tempArray = arrayToFlatten[i];
+      arrayToFlatten.splice(i, 1);
+      for (let j = tempArray.length - 1; j >= 0; j--) {
+        arrayToFlatten.splice(i, 0, tempArray[j]);
+      }
+    }
+  }
+  return arrayToFlatten;
+};
