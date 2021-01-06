3ff1ecb30cd4c56b6ace0946c4d7bd42bacf713e
Isaac King <idrkings@gmail.com>
2021-01-06 20:52:44 +0000

    Function the returns the middle value(s) of an array

---
+++ middle.js
@@ -0,0 +1,18 @@
+const middle = function(array) {
+  let target = checkLength(array);
+  if (target) {
+    return returnIndices(target, array);
+  } else {
+    return [];
+  }
+};
+
+const checkLength = array => {
+  return array.length <= 2 ? false : array.length % 2 === 0 ? 'even' : 'odd';
+};
+
+const returnIndices = (oddOrEven, array) => {
+  let startIndex = Math.floor(array.length / 2);
+  return oddOrEven === 'even' ? [array[startIndex - 1], array[startIndex]] : [array[startIndex]];
+};
+
