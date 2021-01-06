2d7af1a0ef8371a51cd3af27fa5db8367b58ba09
Isaac King <idrkings@gmail.com>
2021-01-06 21:43:14 +0000

    Function to count the occurences of a letter in a sentence

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+const countLetters = function(sentence) {
+ const numberOfOccurences = {};
+ let sentenceNoSpaces = sentence.split(' ').join('');
+
+ for (const letter of sentenceNoSpaces) {
+   if (numberOfOccurences[letter]) {
+     numberOfOccurences[letter]++;
+   } else {
+     numberOfOccurences[letter] = 1;
+   }
+ }
+ return numberOfOccurences;
+};
+
