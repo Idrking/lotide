265e4d5cd16abd27d5ba9158894d3d7eac291276
Isaac King <idrkings@gmail.com>
2021-01-09 02:05:23 +0000

    Added ability to correctly handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,19 @@
 const eqArrays = (firstArray, secondArray) => {
   let arrayLength = 0;
-  if (firstArray.length === secondArray.length) {
+  if (compareLength(firstArray, secondArray)) {
     arrayLength = firstArray.length;
   } else {
     return false;
   }
 
   for (let i = 0; i < arrayLength; i++) {
+    
+    if (Array.isArray(firstArray[i]) && Array.isArray(secondArray[i])) {
+      if (eqArrays(firstArray[i], secondArray[i])) {
+        continue;
+      }
+    }
+    
     if (!(firstArray[i] === secondArray[i])) {
       return false;
     }
@@ -14,3 +21,9 @@ const eqArrays = (firstArray, secondArray) => {
 
   return true;
 };
+
+const compareLength = (firstArray, secondArray) => {
+  return firstArray.length === secondArray.length;
+};
+
+
