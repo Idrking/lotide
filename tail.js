30e38f2ed54a21996d1549e80ee9442d8442aea7
Isaac King <idrkings@gmail.com>
2021-01-11 20:52:34 +0000

    Updated test function to seperate tests

--- tail.js
+++ tail.js
@@ -1,9 +1,3 @@
 const tail = (array) => array.slice(1);
 
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected} ❌`);
-  }
-};
+module.exports = tail;
